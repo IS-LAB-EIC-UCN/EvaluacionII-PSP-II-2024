9eecab3927e6e7f745edd07acb69bb53df158d67
Daniel San Martin <dsanmartins@gmail.com>
2024-11-29 16:24:50 -0300

    first commit

--- org.poliza/src/main/java/org/poliza/ManejadorDefectoPoliza.java
+++ org.poliza/src/main/java/org/poliza/ManejadorDefectoPoliza.java
@@ -1,4 +1,47 @@
 package org.poliza;
 
-public class ManejadorDefectoPoliza {
+import java.util.Calendar;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.*;
+
+class ManejadorDefectoPoliza extends DefaultHandler {
+
+    private Poliza poliza;
+    private String atributo;
+
+    public ManejadorDefectoPoliza(Poliza poliza) {
+        this.poliza = poliza;
+    }
+
+    @Override
+    public void characters(char[] ch, int inicio, int tamanho) {
+        String elemento = new String(ch, inicio, tamanho);
+        this.setAtributoPoliza(elemento);
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        this.atributo = qName;
+    }
+
+    private void setAtributoPoliza(String valor) {
+        if (this.atributo.equals("nombre")) {
+            poliza.setNombre(valor);
+        } else if (this.atributo.equals("apellido")) {
+            poliza.setApellido(valor);
+        } else if (this.atributo.equals("fechaNacimiento")) {
+            Calendar cal = Calendar.getInstance();
+            int mes = Integer.parseInt(valor.substring(0, 2));
+            int dia = Integer.parseInt(valor.substring(3, 5));
+            int anho = Integer.parseInt(valor.substring(6, 10));
+            cal.set(anho, mes - 1, dia);
+            poliza.setFechaNacimiento(cal.getTime());
+        } else if (this.atributo.equals("fumador")) {
+            poliza.setFumador(valor);
+        } else if (this.atributo.equals("estadoCivil")) {
+            poliza.setEstadoCivil(valor);
+        }
+    }
+
 }
--- org.poliza/src/main/java/org/poliza/Poliza.java
+++ org.poliza/src/main/java/org/poliza/Poliza.java
@@ -1,4 +1,47 @@
 package org.poliza;
 
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.StringBufferInputStream;
+import java.util.Date;
+
 public class Poliza {
+    private String nombre;
+    private String apellido;
+    private String fumador;
+    private Date fechaNacimiento;
+    private String estadoCivil;
+
+    public Poliza(String cadenaXml) {
+        try {
+            SAXParserFactory fabrica = SAXParserFactory.newInstance();
+            SAXParser parser = fabrica.newSAXParser();
+            InputSource fuente = new InputSource(new StringBufferInputStream(cadenaXml));
+            parser.parse(fuente, new ManejadorDefectoPoliza(this));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setNombre(String nombre) { this.nombre = nombre; }
+    void setApellido(String apellido) { this.apellido = apellido; }
+    void setFumador(String fumador) { this.fumador = fumador; }
+    void setFechaNacimiento(Date fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+    void setEstadoCivil(String estadoCivil) { this.estadoCivil = estadoCivil; }
+
+    public String getNombre() { return this.nombre; }
+    public Date getFechaNacimiento() { return this.fechaNacimiento; }
+    public String getApellido() { return this.apellido; }
+    public String getEstadoCivil() { return this.estadoCivil; }
+    public String getFumador() { return this.fumador; }
+
+    public void validar() {
+        // Validar los datos.
+    }
+
+    public void guardar() {
+        // Guardar los datos.
+    }
 }
